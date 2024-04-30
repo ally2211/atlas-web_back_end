3199b5f136921283db2915e2be49924694c50202
ally2211 <amendieta.va@gmail.com>
2024-04-30 16:52:58 -0500

    let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Update the variable without redeclaring it
-    let task2 = false;  // Update the variable without redeclaring it
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
