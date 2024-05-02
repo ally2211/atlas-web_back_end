971756469147949c0b4f0ec2ef3978c741e21c85
ally2211 <amendieta.va@gmail.com>
2024-05-02 12:22:02 -0500

    eslint

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,11 +1,11 @@
 // Define the constructor function
 function ClassRoom(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize; // Private-like property
+  this._maxStudentsSize = maxStudentsSize; // Private-like property
 }
 
 // Add a method to the prototype to describe the classroom capacity
 ClassRoom.prototype.describeCapacity = function() {
-    return `This classroom can accommodate up to ${this._maxStudentsSize} students.`;
+  return `This classroom can accommodate up to ${this._maxStudentsSize} students.`;
 };
 
 // Export the ClassRoom as default
