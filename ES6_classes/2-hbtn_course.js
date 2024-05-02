8b466152b1ab2b8a447eb2b9ed1c4b903f47d5d3
ally2211 <amendieta.va@gmail.com>
2024-05-02 17:18:29 -0500

    indents

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,41 +5,41 @@ export default class HolbertonCourse {
     if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new Error('Invalid input types');
     }
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get name() {
-      return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Name must be a string.');
-      }
-      this._name = value;
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string.');
     }
+    this._name = value;
+  }
 
-    get length() {
-      return this._length;
-    }
+  get length() {
+    return this._length;
+  }
 
-    set length(value) {
-      if (typeof value !== 'number') {
-        throw new TypeError('Length must be a number.');
-      }
-      this._length = value;
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number.');
     }
+    this._length = value;
+  }
 
-    get students() {
-      return this._students;
-    }
+  get students() {
+    return this._students;
+  }
 
-    set students(value) {
-      if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-        throw new TypeError('Students must be an array of strings.');
-      }
-      this._students = value;
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings.');
     }
+    this._students = value;
+  }
 }
