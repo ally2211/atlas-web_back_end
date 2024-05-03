1bad8b1d8ceebb470648ee14953582641a02f4f9
ally2211 <amendieta.va@gmail.com>
2024-05-03 05:52:20 -0500

    check parameter firs is undefined

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,7 +1,11 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  // Explicitly check if expansion1989 and expansion2019 are undefined
+  const isExpansion1989Undefined = expansion1989 === undefined;
+  const isExpansion2019Undefined = expansion2019 === undefined;
 
-  const expansion1989Value = expansion1989 === undefined ? 89 : expansion1989;
-  const expansion2019Value = expansion2019 === undefined ? 19 : expansion2019;
+  // Use the check results to determine the values to use
+  const expansion1989Value = isExpansion1989Undefined ? 89 : expansion1989;
+  const expansion2019Value = isExpansion2019Undefined ? 19 : expansion2019;
 
   return initialNumber + expansion1989Value + expansion2019Value;
 }
