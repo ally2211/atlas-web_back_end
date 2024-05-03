36e7bf767a7949dd95f35812ecbb0ac36602e7a9
ally2211 <amendieta.va@gmail.com>
2024-05-03 04:16:23 -0500

    try this.constructor

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -47,6 +47,6 @@ export default class Car {
   // Override the toString method to include both the concise and detailed representation
   // Method for the detailed string representation
   cloneCar() {
-    return new new.target(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
