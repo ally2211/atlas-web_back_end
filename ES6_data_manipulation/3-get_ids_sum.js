a05efc74865e76e20a16ec7b24f476f45263ea05
ally2211 <amendieta.va@gmail.com>
2024-05-05 14:51:15 -0500

    add id's in an array

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,11 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const totalIdSum = students.reduce((accumulator, student) => {
+    return accumulator + student.id;
+    }, 0);
+
+  return totalIdSum;
+}
