0a7877c715a509873ae5877345573229da931f23
ally2211 <amendieta.va@gmail.com>
2024-05-06 01:34:07 -0500

    map items, count

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList(newSet, startString) {
+  const itemMap = new Map();
+  itemMap.set('Apples', 10);
+  itemMap.set('Tomatoes', 10);
+  itemMap.set('Pasta', 1);
+  itemMap.set('Rice', 1);
+  itemMap.set('Banana', 5);
+  return itemMap;
+}  
