b7b0ec4c45b6127af3880f92170b71cfffb8f81d
ally2211 <amendieta.va@gmail.com>
2024-05-06 01:55:16 -0500

    lint if not instance of Map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,14 @@
 export default function updateUniqueItems(itemMap) {
-  for (let [key, value] of itemMap) {
+  // Check if the argument is a Map
+  if (!(itemMap instanceof Map)) {
+    // Throw an error if the argument is not a Map
+    throw new Error('Cannot process: argument is not a Map');
+  }
+  for (const [key, value] of itemMap) {
     if (value === 1) {
       // Update the value to 100
       itemMap.set(key, 100);
     }
   }
   return itemMap;
-}  
+}
