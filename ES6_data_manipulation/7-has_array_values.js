2f8a7e91a2b53917d1cdb52562a13d5a997fe171
ally2211 <amendieta.va@gmail.com>
2024-05-06 02:20:04 -0500

    rewrite for loop to remove ++

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(newSet, newArray) {
-  for (let i = 0; i < newArray.length; i++) {
-    if (!newSet.has(newArray[i])) {
-      return false; // If any element is not found in the set, return false
-    }
-  }
-    return true; // If all elements are found in the set, return true
+  return newArray.every(element => newSet.has(element));
 }
