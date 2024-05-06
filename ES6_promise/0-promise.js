c73ced1ee777675cd35f74da8c10e695f33e04c3
ally2211 <amendieta.va@gmail.com>
2024-05-06 12:01:17 -0500

    use _reject as not yet using it

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _reject) => {
     setTimeout(() => {
       // Resolve the promise with a value
       resolve('API response');
