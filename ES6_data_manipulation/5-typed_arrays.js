2f4ea7d68286421a4981e0a4eebea4a619dcfd13
ally2211 <amendieta.va@gmail.com>
2024-05-06 03:03:09 -0500

    lint

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
   // Create a DataView that references the ArrayBuffer
   const dataView = new DataView(buffer);
   const int8View = new Int8Array(buffer); // Creates an Int32Array view of the buffer
-  if (position < (length -1) || position < 0) {
+  if (position < length || position < 0) {
     int8View[position] = value;
   } else {
     throw new Error('Position outside range');
