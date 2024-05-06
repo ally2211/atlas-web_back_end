cdd531ecce3bc43aecce3c4b414628a68c8f212b
ally2211 <amendieta.va@gmail.com>
2024-05-06 12:08:08 -0500

    remove reject for lint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, _reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       // Resolve the promise with a value
       resolve('API response');
