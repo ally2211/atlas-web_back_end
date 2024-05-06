ae6218785fb9857ce5dc3c25495e364ce8e018ff
ally2211 <amendieta.va@gmail.com>
2024-05-06 04:30:25 -0500

    return empty string not error

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,11 @@
 export default function cleanSet(newSet, startString) {
   const modifiedElements = [];
   // Check if startString is not a string or is an empty string
-  if (typeof startString === 'undefined' || startString === '' ) {
+  if (typeof startString === 'undefined' || startString === '') {
     return '';
   }
   if (typeof startString !== 'string') {
-    // Return an empty string if the condition is met
-    throw new TypeError('startString must be a non-empty string');
+    return '';
   }
   newSet.forEach((element) => {
     if (element.startsWith(startString)) {
