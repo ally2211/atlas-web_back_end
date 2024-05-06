3b8f08c6e851349d739ec072cb536ef824b9c7f7
ally2211 <amendieta.va@gmail.com>
2024-05-06 03:10:54 -0500

    lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,16 +4,15 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   }
 
   const locationStudents = students.filter((student) => student.location === city);
-  //console.log(locationStudents);
-  //console.log(newGrades);
 
   const updatedStudents = locationStudents.map((student) => {
-    const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+    const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
     if (matchingGrade) {
-      //console.log(matchingGrade.grade);
       return { ...student, grade: matchingGrade.grade };
     }
-    else return { ...student, grade: 'N/A' };;
+    else {
+      return { ...student, grade: 'N/A' };
+    }
   });
 
   return updatedStudents;
