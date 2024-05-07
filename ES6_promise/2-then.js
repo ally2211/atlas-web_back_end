3f7136e32c687f8cae5d86cab9343f09796693f1
ally2211 <amendieta.va@gmail.com>
2024-05-07 13:42:31 -0500

    lower case success

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -4,7 +4,7 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API'); // Log the success message
       return {
         status: 200,
-        body: 'Success'
+        body: 'success'
       };
     }).catch((error) => {
       return Promise.reject(error);
