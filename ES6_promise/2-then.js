9db18ff0e4515a113e6acb7a49bc60469d22c166
ally2211 <amendieta.va@gmail.com>
2024-05-07 12:56:53 -0500

    pass a promise as an argument

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 // 1-promise.js
-export default function getFullResponseFromAPI(success) {
+export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    if (success) {
+    if (promise) {
       // Resolve the promise with an object
       resolve({
         status: 200,
-        body: 'success',
+        body: 'Success',
       });
       console.log('Got a response from the API');
     } else {
