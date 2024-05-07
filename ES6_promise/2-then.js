cfecb01a4c2a345cbec960d495768146a2923f51
ally2211 <amendieta.va@gmail.com>
2024-05-07 13:52:48 -0500

    lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   if (promise instanceof Promise) {
-    return promise.then((response) => {
+    return promise.then(() => {
       console.log('Got a response from the API'); // Log the success message
       return {
         status: 200,
-        body: 'success'
+        body: 'success',
       };
     }).catch((error) => {
-      throw error; // Re-throw the error to maintain rejection
+      return Promise.reject(new Error());
     });
-  } else {
-    // If the passed argument is not a promise, reject with an error
-    return Promise.reject(new Error());
   }
 }
