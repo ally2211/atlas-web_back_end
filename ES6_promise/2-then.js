f408ab33b054278c6a69ec5ccddc8ed8cbe3d5c3
ally2211 <amendieta.va@gmail.com>
2024-05-07 13:37:55 -0500

    remove new promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,16 @@
-// 2=then.js
 export default function handleResponseFromAPI(promise) {
   if (promise instanceof Promise) {
-    return new Promise((resolve, reject) => {
-      promise.then(() => {
-        resolve({
-          status: 200,
-          body: 'Success'
-        });
-      }).catch((error) => {
-        reject(error);
-      });
+    return promise.then((response) => {
+      console.log('Got a response from the API'); // Log the success message
+      return {
+        status: 200,
+        body: 'Success'
+      };
+    }).catch((error) => {
+      return Promise.reject(error);
     });
-    } else {
-      // If the passed argument is not a promise, reject the new promise with an error
-      return Promise.reject(new Error());
-    };
+  } else {
+    // If the passed argument is not a promise, reject with an error
+    return Promise.reject(new Error());
+  }
 }
