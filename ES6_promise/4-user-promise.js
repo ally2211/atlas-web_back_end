2e63d39948c11c8cddfaf56d4357f3409dc27fc0
ally2211 <amendieta.va@gmail.com>
2024-05-08 17:59:44 -0500

    take out reject from promise

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,15 +1,12 @@
 export default function handlePrsignUpUser(firstName, lastName) {
   // Example asynchronous operation
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (firstName && lastName) {
       // Resolve the promise with an object
       resolve({
         firstName: 'Bob',
         lastName: 'Dylan',
       });
-    } else {
-      // Reject the promise with an error
-      reject(new Error());
     }
   });
 }
