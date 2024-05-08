4abdbac6b709059c73a39a7ca4dede5375ea06c0
ally2211 <amendieta.va@gmail.com>
2024-05-07 23:19:56 -0500

    utils

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,18 @@
+import * as utilsFunctions from "./utils";
+
+export default function handleProfileSignup() {
+ // Collect all promises
+ const promises = [utilsFunctions.uploadPhoto(), utilsFunctions.createUser()];
+
+ // Use Promise.all to wait for all promises to resolve
+ Promise.all(promises)
+    .then((results) => {
+      // results is an array of resolved values
+      // Assuming the first result is from uploadPhoto and the second is from createUser
+      const photoResult = results[0];
+      const userResult = results[1];
+      // Log the body, firstName, and lastName to the console
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    .catch(() => new Error('Signup system offline'));
+}
--- ES6_promise/utils.js
+++ ES6_promise/utils.js
@@ -2,17 +2,19 @@
 export function uploadPhoto() {
  // Simulate an asynchronous operation
  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ body: 'photo-profile-1' });
-    }, 1000);
+   resolve({
+     status: 200,
+     body: 'photo-profile-1',
+   });
  });
 }
 
 export function createUser() {
  // Simulate an asynchronous operation
  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ firstName: 'Guillaume', lastName: 'Salva' });
-    }, 1000);
+   resolve({
+     firstName: 'Guillaume',
+     lastName: 'Salva',
+   });
  });
 }
