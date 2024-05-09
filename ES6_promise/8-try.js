748da43b0921e140e23d2b1cf3a0bb64f5c8258e
ally2211 <amendieta.va@gmail.com>
2024-05-08 22:21:15 -0500

    error div by 0

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,12 +1,13 @@
 export default function divideFunction(numerator, denominator) {
   // Example asynchronous operation
   return new Promise((resolve) => {
+    if (denominator == 0) {
+        return Promise.reject(new Error('cannot divide by 0'));
+    }
     if (numerator && denominator) {
       // Resolve the promise with an object
-      resolve({
-        numerator,
-        denominator,
-      });
+      const div = numerator / denominator;
+      console.log(div);
     }
   });
 }
