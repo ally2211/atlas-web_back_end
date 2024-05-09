6dbaddc5f1784e7791b1762426ff05197684083e
ally2211 <amendieta.va@gmail.com>
2024-05-08 22:35:32 -0500

    resolve

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,13 +1,14 @@
 export default function divideFunction(numerator, denominator) {
   // Example asynchronous operation
   return new Promise((resolve) => {
-    if (denominator == 0) {
-        return Promise.reject(new Error('cannot divide by 0'));
+    if (denominator === 0) {
+      return Promise.reject(new Error('cannot divide by 0'));
     }
     if (numerator && denominator) {
       // Resolve the promise with an object
-      const div = numerator / denominator;
-      console.log(div);
+      const result = numerator / denominator;
+      resolve({ result });
     }
+    return undefined;
   });
 }
