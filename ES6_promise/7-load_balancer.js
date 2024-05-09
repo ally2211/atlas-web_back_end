0b2109bba113dd095d782bdf72832f873b517bd5
ally2211 <amendieta.va@gmail.com>
2024-05-08 22:03:51 -0500

    lint

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,15 +1,13 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.all([chinaDownload, USDownload]).then((results) => {
-    const chinaResult = results[0];
-    const USResult = results[1];
-    
+  return Promise.all([chinaDownload, USDownload]).then(() => {
     chinaDownload.then((chinaResult) => {
       console.log(chinaResult);
-    // After logging the result of the first promise, execute the second promise
-    USDownload.then((USResult) => {
-      console.log(USResult);
-    })}
-  )}).catch((error) => {
+      // After logging the result of the first promise, execute the second promise
+      USDownload.then((USResult) => {
+        console.log(USResult);
+      });
+    });
+  }).catch((error) => {
     console.log('Signup system offline', error);
   });
 }
