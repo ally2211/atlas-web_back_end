42173aa9df8e528e6660fa3af5eef1643f3cd8f4
ally2211 <amendieta.va@gmail.com>
2024-08-27 00:19:45 -0500

    pycodestyle and documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This module provides utilities for mathematical operations
+"""
 
-def add(x:float, y:float) -> float:
+def add(x: float, y: float) -> float:
+    """
+    Add two numbers together
+    """
     return x + y
