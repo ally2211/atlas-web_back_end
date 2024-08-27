e9db5c95afe3e69adace246b4344b2f038835115
ally2211 <amendieta.va@gmail.com>
2024-08-27 06:11:06 -0500

    return floor of float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+"""
+This module provides utilities for getting the floor of a float
+"""
+
+
+def floor(x: float) -> int:
+    """
+    get floor of a float
+    """
+    return math.floor(x)
