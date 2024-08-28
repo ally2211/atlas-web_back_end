092c6d1b3969b4b470ba78926703baaff7fb337b
ally2211 <amendieta.va@gmail.com>
2024-08-28 01:09:56 -0500

    pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,8 @@ Complex types - functions
 from typing import List, Iterable, Tuple, Sequence
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence,int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+    return length of list
+    '''
     return [(i, len(i)) for i in lst]
