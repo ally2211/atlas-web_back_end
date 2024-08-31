302fcdc5eba2d47427ea8452b201495cb6410760
ally2211 <amendieta.va@gmail.com>
2024-08-31 10:55:41 -0500

    pycodestyle

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,12 +10,16 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     '''
-    measures the total execution time 
+    measures the total execution time
     '''
-     # Run the asynchronous code in an event loop and get the result
+    # Run the asynchronous code in an event loop and get the result
     return asyncio.run(_measure_time_async(n, max_delay))
 
+
 async def _measure_time_async(n: int, max_delay: int) -> float:
+    '''
+    does await to get total execution time
+    '''
     delays = await wait_n(n, max_delay)
     total_time = sum(delays)
     return total_time / n
