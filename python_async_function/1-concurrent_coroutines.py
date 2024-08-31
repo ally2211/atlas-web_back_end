927e37cd5f07c39af16b86e5b77cd5455456ee3d
ally2211 <amendieta.va@gmail.com>
2024-08-31 10:01:45 -0500

    pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,21 +4,22 @@ The basics of async
 '''
 import asyncio
 import random
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List, Iterable
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''
-    return a list that calls wait_random and returns random delays that are sorted
+    return a list that calls wait_random and returns
+    random delays that are sorted
     '''
-    tasks=[]
+    tasks = []
     for i in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         tasks.append(task)
     delays = await asyncio.gather(*tasks)
-    
-     # Implementing selection sort to sort the list
+
+    # Implementing selection sort to sort the list
     for i in range(len(delays)):
         min_index = i
         for j in range(i + 1, len(delays)):
