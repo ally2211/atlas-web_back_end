e14a0cc472bc68582e3ebb89926c617a8e75ea8f
ally2211 <amendieta.va@gmail.com>
2024-08-31 19:28:43 -0500

    run asyncio.gather in parallel

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+  Async Comprehensions
+'''
+import asyncio
+import random
+from typing import List
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    returns sum of lists
+    '''
+    start_time = time.perf_counter()  # Record start time
+    # Run four instances of async_comprehension in parallel
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    
+    end_time = time.perf_counter()  # Record end time
+    
+    # Calculate total runtime
+    total_sum = end_time - start_time
+   
+    return total_sum
