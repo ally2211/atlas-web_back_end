ec94d871161c3a5277b426ab7f0ac4401abb79e1
ally2211 <amendieta.va@gmail.com>
2024-09-05 14:14:40 -0500

    pycodestyle

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ basic_cache.py """
 from base_caching import BaseCaching
-#BaseCaching = __import__('BaseCaching').BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -9,21 +8,21 @@ class BasicCache(BaseCaching):
     key value pairs."""
     def __init__(self):
         """Initialize the parent class"""
-        super().__init__()  
+        super().__init__()
         self.cache_data = {}  # Initialize an empty dictionary for caching
 
     def put(self, key, item):
         """Add key-item pair to cache."""
         if key is None or item is None:
             return
-        
+
         # Store the item in the cache
         self.cache_data[key] = item
-        
+
     def get(self, key):
         """Retrieve item from cache by key."""
         if key is None:
             return None
-        
+
         # Return the item if it exists in the cache
         return self.cache_data.get(key, None)
