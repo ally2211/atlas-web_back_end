48b0ff612da08dcff752e5d30ce10980e723881e
ally2211 <amendieta.va@gmail.com>
2024-09-05 12:09:10 -0500

    Basic put and get to dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" basic_cache.py """
+BaseCaching = __import__('BaseCaching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """class inherits and overrides put and get to dictionary
+    key value pairs."""
+    def __init__(self):
+        """Initialize the parent class"""
+        super().__init__()  
+        self.cache_data = {}  # Initialize an empty dictionary for caching
+
+    def put(self, key, item):
+        """Add key-item pair to cache."""
+        if key is None or item is None:
+            return
+        
+        # Store the item in the cache
+        self.cache_data[key] = item
+        
+    def get(self, key):
+        """Retrieve item from cache by key."""
+        if key is None:
+            return None
+        
+        # Return the item if it exists in the cache
+        return self.cache_data.get(key, None)
