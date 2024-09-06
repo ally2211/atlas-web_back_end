d49d866440ab3ed5e8bced1a8418b889f8490267
ally2211 <amendieta.va@gmail.com>
2024-09-05 23:58:11 -0500

    documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,9 @@ Simple helper function
 
 
 def index_range(page, page_size):
+    """
+    create pagination tuple
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     tupleindex = (start_index, end_index)
