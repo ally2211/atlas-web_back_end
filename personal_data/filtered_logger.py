789860e29c59c03e2c8da70981028090a27b93d6
ally2211 <amendieta.va@gmail.com>
2024-09-11 00:03:53 -0500

    import typing  List

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,18 +3,19 @@
 Regex
 """
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     '''
     Obfuscates values in the specified fields from the message.
-    '''    # Create a pattern based on the fields to be obfuscated
+    '''
+    # Create a pattern based on the fields to be obfuscated
     for field in fields:
-        # Construct the regex pattern for the field to match everything after the key until the next separator
+        # regex pattern to match until the next separator
         pattern = rf'{field}=[^;]+'
-        # Use re.sub() to replace the matched field's value with the redaction string
+        # Use re.sub()  with the redaction string
         message = re.sub(pattern, f'{field}={redaction}', message)
 
     return message
-
