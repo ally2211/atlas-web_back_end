d76b245548afad51e7ead0375fe2f2dcfafa6f61
ally2211 <amendieta.va@gmail.com>
2024-09-12 19:01:43 -0500

    retry

--- personal_data/main.py
+++ personal_data/main.py
@@ -3,18 +3,20 @@
 Main file
 """
 
-filter_datum = __import__('filtered_logger').filter_datum
+import logging
+import re
 
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+print(formatter.format(log_record))
+
 
 redaction = "bbbbbb"
 separator = "&"
 fields1 = ["password", "ssn", "phone"]
 message1 = "name=User&password=password&email=user@example.com&ssn=12345&phone=411&"
 s1 = "name=User&password=bbbbbb&email=user@example.com&ssn=bbbbbb&phone=bbbbbb&"
-print(filter_datum(fields1, redaction, message1, separator))
-print("filter_datum worked as expected: {}".format(filter_datum(fields1, redaction, message1, separator) == s1))
+print(formatter.format(log_record))
--- personal_data/main1.py
+++ personal_data/main1.py
@@ -12,3 +12,11 @@ message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
 log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
 formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 print(formatter.format(log_record))
+
+
+redaction = "bbbbbb"
+separator = "&"
+fields1 = ["password", "ssn", "phone"]
+message1 = "name=User&password=password&email=user@example.com&ssn=12345&phone=411&"
+s1 = "name=User&password=bbbbbb&email=user@example.com&ssn=bbbbbb&phone=bbbbbb&"
+print(formatter.format(log_record))
