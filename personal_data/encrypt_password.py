d18c367983b09bc0a434a4707693aa56ee46b158
ally2211 <amendieta.va@gmail.com>
2024-09-18 18:43:44 -0500

    pycodestyle

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,19 +5,19 @@ encrypt a password
 import bcrypt
 
 
-def hash_password (password: str) -> bytes:
+def hash_password(password: str) -> bytes:
     '''
     get a password as a string and return a salted hashed password
     which is a byte string
     '''
     # Convert the password to bytes
     password_bytes = password.encode('utf-8')
-    
+
     # Generate a salt
     salt = bcrypt.gensalt()
-    
+
     # Hash the password using bcrypt.hashpw
     hashed_password = bcrypt.hashpw(password_bytes, salt)
-    
+
     # Return the salted, hashed password (as a byte string)
     return hashed_password
