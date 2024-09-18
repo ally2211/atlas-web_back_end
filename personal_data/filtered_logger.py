1dca4b107e23fdb3196e43129d26fb8daaeafdf5
ally2211 <amendieta.va@gmail.com>
2024-09-18 13:51:21 -0500

    create env variables

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,19 +2,29 @@
 """
 connection to sql database
 """
+import os
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
-from typing import Optional
+
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
+    '''
+    establish mysql connection using environment variables
+    '''
+    # Fetching environment variables with defaults if not set
+    db_user: str = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    db_password: str = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host: str = os.getenv('PERSONAL_DATA_DB_HOST', 'host.docker.internal')
+    db_name: str = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
+
     try:
         # Establish connection
         connection: MySQLConnection = mysql.connector.connect(
-            host='localhost', # 'host.docker.internal'
-            user='root',
-            password='',
-            database='my_db'
+            host=db_host, # 'host.docker.internal'
+            user=db_user,
+            password=db_password,
+            database=db_name
         )
         if connection.is_connected():
             # print("Connected to MySQL database")
