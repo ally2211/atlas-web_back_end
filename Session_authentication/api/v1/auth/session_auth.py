e640063c3cd1604a494a0a556386de1ce141c1df
ally2211 <amendieta.va@gmail.com>
2024-09-22 18:07:25 -0500

    bin

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
 from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     # Inherit from Auth, without overloading any methods for now
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        instance of method createsession
+        '''
+        pass
+        
