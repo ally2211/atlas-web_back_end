ba30dee0e5e9bcec1c7fec9237423a5779be9dbd
ally2211 <amendieta.va@gmail.com>
2024-09-22 19:43:44 -0500

    userid for sessionid

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -25,10 +25,21 @@ class SessionAuth(Auth):
         
         # Generate a unique session ID
         session_id = str(uuid.uuid4())
-        
+        print(session_id) 
         # Store the session ID as a key and user_id as the value
         self.user_id_by_session_id[session_id] = user_id
         
         return session_id
 
-    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID.
+        Returns None if session_id is None or not a string.
+        Returns the value (User ID) for the key session_id in the dictionary user_id_by_session_id.
+        """
+        # Return None if session_id is None or not a string
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        
+        # Use .get() to access the user_id based on session_id in the dictionary
+        return self.user_id_by_session_id.get(session_id)
