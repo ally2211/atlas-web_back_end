3feeeb222764e2c421ea9163c92002435f5039aa
ally2211 <amendieta.va@gmail.com>
2024-09-24 23:10:10 -0500

    parameterize

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,20 +10,7 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     '''
     method to test that the method returns what it is supposed to
-    '''
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    
-    def test_access_nested_map(self, nested_map, path, expected):
-        '''
-        test parameterized nested maps
-        '''
-        result = access_nested_map(nested_map, path)
-        self. assertEqual(result, expected)
-        
+    '''      
     @parameterized.expand([
         ({}, ("a",)),                     # Test case 1: empty dictionary
         ({"a": 1}, ("a", "b")),           # Test case 2: key "a" exists, "b" does not
