eb3b61411b46e8ab15f3201b54edaa7478de558d
ally2211 <amendieta.va@gmail.com>
2024-09-25 16:51:08 -0500

    added expected to key error

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,15 +10,28 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     '''
     method to test that the method returns what it is supposed to
-    '''      
+    '''
     @parameterized.expand([
-        ({}, ("a",)),                     # Test case 1: empty dictionary
-        ({"a": 1}, ("a", "b")),           # Test case 2: key "a" exists, "b" does not
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-        
-    def test_access_nested_map_key_error(self, nested_map, path):
+    
+    def test_access_nested_map(self, nested_map, path, expected):
+        '''
+        test parameterized nested maps
+        '''
+        result = access_nested_map(nested_map, path)
+        self. assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),                     # Test case 1: empty dictionary
+        ({"a": 1}, ("a", "b"), KeyError),           # Test case 2: key "a" exists, "b" does not
+    ])
+
+    def test_access_nested_map_key_error(self, nested_map, path, expected_exception):
         """Test that access_nested_map raises KeyError for invalid keys."""
-        with self.assertRaises(KeyError):
+        with self.assertRaises(expected_exception):
             access_nested_map(nested_map, path)
 
 
