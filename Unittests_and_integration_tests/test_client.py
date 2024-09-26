9ec795ff4e8f2babce7d6f6d363831002735f387
ally2211 <amendieta.va@gmail.com>
2024-09-25 22:22:02 -0500

    pycode

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from client import GithubOrgClient 
+from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -11,12 +12,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",)
     ])
-    @patch('client.get_json', return_value={"login": "test-org"})  # Mock get_json to return a dummy organization
+    # Mock get_json to return a dummy organization
+    @patch('client.get_json', return_value={"login": "test-org"})
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the correct value"""
         client = GithubOrgClient(org_name)
         org = client.org  # Call 'org' as a property or attribute, not a method
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(org, {"login": "test-org"})
 
 
