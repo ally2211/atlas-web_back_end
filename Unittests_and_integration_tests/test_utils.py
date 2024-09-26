34f4c666ffbf1080c829c50215a28a9a94450d0c
ally2211 <amendieta.va@gmail.com>
2024-09-25 19:19:34 -0500

    3. Parameterize and patch

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map
 from utils import get_json
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,6 +48,7 @@ class TestGetJson(unittest.TestCase):
     ("http://example.com", {"payload": True}),
     ("http://holberton.io", {"payload": False}),
     ])
+    
     @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         '''
@@ -65,7 +67,53 @@ class TestGetJson(unittest.TestCase):
         # assert that the returned value from the get_json is the test payload
         self.assertEqual(result, test_payload)
 
+ 
+# Define TestClass outside the test function so that it can be patched
+class TestClass:
+    def a_method(self):
+        """
+        A simple method that returns 42.
+        This method simulates some operation that we want to cache the result of.
+        """
+        return 42
+
+    @memoize
+    def a_property(self):
+        """
+        This method returns the result of a_method.
+        Because it is decorated with @memoize, the result will be cached
+        after the first call, so subsequent calls will not re-execute a_method.
+        """
+        return self.a_method()
 
 
+class TestMemoize(unittest.TestCase):
+
+    @patch.object(TestClass, 'a_method')  # Patch a_method of TestClass
+    def test_memoize(self, mock_a_method):
+        """
+        Test that a_method is only called once when memoization is applied,
+        even when a_property is called twice.
+        """
+        # Set the return value of the mock for a_method to 42
+        mock_a_method.return_value = 42
+
+        # Create an instance of TestClass
+        obj = TestClass()
+
+        # Call a_property twice
+        first_call = obj.a_property
+        second_call = obj.a_property
+
+        # Assert that a_method was called once
+        mock_a_method.assert_called_once()
+
+        # Assert that both calls return the correct result
+        self.assertEqual(first_call, 42)
+        self.assertEqual(second_call, 42)
+
+        # Assert that both calls return the same cached result (i.e., memoization works)
+        self.assertIs(first_call, second_call)   
+    
 if __name__=="__main__":
     unittest.main()
