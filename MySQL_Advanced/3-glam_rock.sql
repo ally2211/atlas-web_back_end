e620d102792e5336aeb2c4a80fac0871fd31b5ee
ally2211 <amendieta.va@gmail.com>
2024-10-07 20:27:17 -0500

    abs

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- now count
 SELECT band_name, 
-10 as lifespan
+isnull(ABS(split - formed), 0) as lifespan
 FROM metal_bands
-WHERE style like '%Glam rock%'
+WHERE style = 'Glam rock'
 GROUP BY band_name
 ORDER BY lifespan desc, band_name desc;
