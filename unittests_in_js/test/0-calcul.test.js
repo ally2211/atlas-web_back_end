9f1a5e913f7d4f4c6c4b8e72e70b56978723b82d
ally2211 <amendieta.va@gmail.com>
2024-10-16 03:16:41 -0500

    unittests

---
+++ unittests_in_js/test/0-calcul.test.js
@@ -0,0 +1,12 @@
+// test/rounding.test.js
+const assert = require('assert');
+const calculateNumber = require('../0-calcul.js');
+
+describe('calculateNumber', function () {
+  it('should return the sum of the rounded numbers', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4); 
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6); 
+  });
+});
