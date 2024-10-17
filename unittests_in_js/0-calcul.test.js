f25d928b04a35e63c642122ce53f922b76b98149
ally2211 <amendieta.va@gmail.com>
2024-10-17 18:47:32 -0500

    add assert test for 2nd number

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,6 +7,7 @@ describe('calculateNumber', function () {
     assert.strictEqual(calculateNumber(1, 3), 4); 
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6); 
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(2, 2.6), 5);
   });
 });
