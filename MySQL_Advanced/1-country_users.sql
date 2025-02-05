7a9cf1e2a2042d214ab0bbbe118869ce7d21ce5e
Allyson E Ugarte <amendieta.va@gmail.com>
2025-02-05 12:58:24 -0600

    Delete MySQL_Advanced directory

--- MySQL_Advanced/1-country_users.sql
+++
@@ -1,8 +0,0 @@
--- create table for users if not exists
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-    PRIMARY KEY (id)
-);
