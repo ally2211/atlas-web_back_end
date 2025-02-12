aa584093f0874fe097969ec3fb62edd9b976585b
ally2211 <amendieta.va@gmail.com>
2025-02-12 03:44:25 -0600

    create a table

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+--create a table for users if not exists
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
