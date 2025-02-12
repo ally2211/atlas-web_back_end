6fa4e98d808b51da4bf064450e531adcea46c591
ally2211 <amendieta.va@gmail.com>
2025-02-12 04:13:38 -0600

    use where like

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 SELECT band_name,
 (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan 
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style like '%Glam rock%'
 GROUP BY band_name
 ORDER BY lifespan desc, band_name desc;
