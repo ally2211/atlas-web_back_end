df0132d14786b53af050ea61336f1bef5e67ce2e
ally2211 <amendieta.va@gmail.com>
2025-02-12 04:00:16 -0600

    create index and groupings

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- first create a table index on origin and then perform count
+CREATE INDEX idx_origin ON metal_bands(origin);
+-- now count
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans desc, origin asc;
