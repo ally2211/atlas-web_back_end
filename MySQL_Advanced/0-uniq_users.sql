1432d0cafb61d779957ae1133a6fdc9aac0c83d0
ally2211 <amendieta.va@gmail.com>
2025-02-12 03:49:21 -0600

    no primary key

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
---create a table for users if not exists
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
+    name VARCHAR(255)
 );
