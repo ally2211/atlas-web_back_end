c54cc5622b85c87b1f01101e1e281cd6ac3dfc8c
ally2211 <amendieta.va@gmail.com>
2025-02-15 18:25:04 -0600

    params in sql

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
+    IN user_id_param  INT
 )
 BEGIN
     DECLARE avg_score DECIMAL(10,2);
@@ -12,12 +12,12 @@ BEGIN
     -- Compute the average score for the given user_id
     SELECT AVG(score) INTO avg_score 
     FROM corrections 
-    WHERE user_id = user_id;
+    WHERE user_id = user_id_param ;
 
     -- Update the user's average score in the users table
     UPDATE users 
     SET average_score = avg_score 
-    WHERE id = user_id;
+    WHERE id = user_id_param ;
 END$$
 
 DELIMITER ;
