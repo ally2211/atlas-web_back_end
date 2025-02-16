600032cebd4b7c133e9947969d74906c6ed809bb
ally2211 <amendieta.va@gmail.com>
2025-02-15 19:12:31 -0600

    sql script to create a function

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- a SQL script that creates a function SafeDiv that divides (and returns) the first by the 
+-- second number or returns 0 if the second number is equal to 0.
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT) 
+RETURNS DECIMAL(10,2) 
+DETERMINISTIC
+BEGIN
+    RETURN IF(b = 0, 0, a / b);
+END$$
+
+DELIMITER ;
