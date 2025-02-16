82053e46ffbfb93064c97b217085540a5159cf59
ally2211 <amendieta.va@gmail.com>
2025-02-15 23:05:06 -0600

    list all using python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """Lists all documents in a MongoDB collection."""
+    return list(mongo_collection.find()) if mongo_collection.count_documents({}) > 0 else []
