ff1c8b55b4f402ae7d32b3b27dfa83087815c867
ally2211 <amendieta.va@gmail.com>
2025-02-15 19:53:49 -0600

    subtract 1

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- count years as split - formed
 SELECT band_name,
-(IFNULL(split, YEAR(CURDATE())) - IFNULL(formed, YEAR(CURDATE()))) AS lifespan 
+(IFNULL(split, YEAR(CURDATE())) - IFNULL(formed, YEAR(CURDATE()) -1)) AS lifespan 
 FROM metal_bands
 WHERE TRIM(LOWER(style)) LIKE '%glam rock%'
 ORDER BY lifespan DESC;
